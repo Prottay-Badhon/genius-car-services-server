0c760ff2cbe9bc419f5b13ea1ad0c5e209697b34
Prottay-Badhon <prottaybadhon54@gmail.com>
2023-02-07 18:05:16 +0600

    first commit

---
+++ _steps.js
@@ -0,0 +1,28 @@
+/* 
+1. npm init -y
+2. Make index.js file
+3. npm i express cors mongodb dotenv
+4. go to index.js file and follow this instruction
+    4.1 Require express
+    4.2 Require cors
+    4.3 Require dotenv
+    4.4 Call express function
+    4.5 use express.json() for parsing
+    4.6 set port number 5000
+    4.7 listen port
+    4.8 edit scripts in package.json
+             "start": "node index.js",
+             "start-dev": "nodemon index.js",
+    4.9 nodemon index.js to start server  
+ 5. Connect with MongoDB
+    5.1 Database->connect->copy code
+    5.2 paste the code into index.js
+    5.3 set user and password and replace 
+ 
+ 6. Find data from collections
+    6.1 erase client.connect 
+    6.2 make async run function with try and finally 
+    6.3 call run function with .catch with console.dir parameter
+    
+
+*/
